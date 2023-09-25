f708825eee3d0fbebdf3aa85bed8896a9d7eacf8
Chinhxvn <Chinhxvn@gmail.com>
2023-09-25 14:41:49 -0400

    Added the password checker script

--- password_checker.py
+++ password_checker.py
@@ -2,3 +2,17 @@
 #This is a script for testing passwords 
 #Test
 
+def password_length_checker(input_string):
+	return len(input_string) >= 8 
+
+while True:
+	password = input("Enter your password that is 8 characters long: ") 
+	
+	if password_length_checker(password):
+		print("Your password is 8 characters long")
+		break
+	else:
+		print("Your password is not long enough, please try again. ") 
+
+print("Your password is:", password)
+
